70ed81c3b3d3505538aae558278d7cfbb1dafe0e
Amisha Govind Gayatri Prabhu <93538204+amishaprabhu@users.noreply.github.com>
2022-11-15 15:05:07 +0530

    Update Second max difference in array.java

--- Second max difference in array.java
+++ Second max difference in array.java
@@ -1,9 +1,6 @@
 /******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+Calculating the second maximum difference in an unsorted array.
 
 *******************************************************************************/
 import java.util.*;
