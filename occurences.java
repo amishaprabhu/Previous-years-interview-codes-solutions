a706b5c894e875464a3b1472c4d0add375aab2fe
Amisha Govind Gayatri Prabhu <93538204+amishaprabhu@users.noreply.github.com>
2022-12-31 23:49:36 +0530

    Create occurences.java

---
+++ occurences.java
@@ -0,0 +1,6 @@
+Input string is provided, task is to count no of occurences of each character in the string and get the counts which are prime no. 
+Calculate the sum of all prime no counts value and calculate the multiplication of all prime no counts value and print them. 
+
+Code:
+Input:
+Ouptut:
